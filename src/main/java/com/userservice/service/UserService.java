29cf4e02d1fc0280b816d5b73e084eeaa0f3b2fd
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-06-12 19:25:41 -0500

    Initial implementation of the project with the following features: - Backend developed using Java and Spring Boot. - Database integration with SQL. - Dependency management and build automation using Maven. - Core functionalities implemented and tested.

---
+++ src/main/java/com/userservice/service/UserService.java
@@ -0,0 +1,45 @@
+package com.userservice.service;
+
+import com.userservice.model.User;
+import com.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> updateUser(Long id, User userDetails) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setName(userDetails.getName());
+                    user.setEmail(userDetails.getEmail());
+                    return userRepository.save(user);
+                });
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
